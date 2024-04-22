b8a8efe1747455e33cd5467874a736445e4bd97f
cyburns <72774499+cyburns@users.noreply.github.com>
2024-04-22 12:49:04 -0400

    theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import ThemeContextProvider from "@/context/ThemeContext";
+import ThemeSwitch from "@/components/ThemeSwitch";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -16,7 +18,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <ThemeContextProvider>
+          {children}
+          <ThemeSwitch />
+        </ThemeContextProvider>
+      </body>
     </html>
   );
 }
